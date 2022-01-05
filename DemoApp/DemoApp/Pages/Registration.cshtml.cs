6764432b60b82c98fda2be21a04fb03c0b221f73
yedu.madhumanchi <yedu.madhumanchi@yondrgroup.com>
2022-01-06 00:23:26 +0530

    Application insights implemnted

--- DemoApp/DemoApp/Pages/Registration.cshtml.cs
+++ DemoApp/DemoApp/Pages/Registration.cshtml.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 
 namespace DemoApp.Pages
@@ -16,9 +17,11 @@ namespace DemoApp.Pages
         [BindProperty]
         public DemoApp.Models.Registration RegistrationUser { get; set; }
         private readonly IConfiguration _configuration;
-        public RegistrationModel(IConfiguration configuration)
+        private readonly ILogger _logger;
+        public RegistrationModel(IConfiguration configuration, ILogger logger)
         {
             _configuration = configuration;
+            _logger = logger;
         }
         public void OnGet()
         {
@@ -26,19 +29,29 @@ namespace DemoApp.Pages
 
         public ActionResult OnPost()
         {
-            var json = JsonConvert.SerializeObject(RegistrationUser);
-            var client = new HttpClient();
-            HttpContent content = new StringContent(json, Encoding.UTF8, "application/json");
-            string path = $"{_configuration["AppSettings:BaseAPI"]}api/Registration";
-            var res = client.PostAsync(path, content).GetAwaiter().GetResult();
-            if(res.IsSuccessStatusCode)
+            try
             {
-                return RedirectToPage("Index");
+                _logger.LogInformation("Registration process has been started.");
+                var json = JsonConvert.SerializeObject(RegistrationUser);
+                var client = new HttpClient();
+                HttpContent content = new StringContent(json, Encoding.UTF8, "application/json");
+                string path = $"{_configuration["AppSettings:BaseAPI"]}api/Registration";
+                var res = client.PostAsync(path, content).GetAwaiter().GetResult();
+                if (res.IsSuccessStatusCode)
+                {
+                    return RedirectToPage("Index");
+                }
+                else
+                {
+                    return RedirectToPage("Registration");
+                }
             }
-            else
+            catch (Exception)
             {
-                return RedirectToPage("Registration");
+
+                throw;
             }
+           
         }
     }
 }
